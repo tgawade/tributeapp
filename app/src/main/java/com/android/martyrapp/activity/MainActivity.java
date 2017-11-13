b26fd13578a0b91a6306d0bc011c56f18cf8b308
tgawade <tushar1577@gmail.com>
2017-11-13 21:26:33 +0530

    added retrofit

--- app/src/main/java/com/android/martyrapp/activity/MainActivity.java
+++ app/src/main/java/com/android/martyrapp/activity/MainActivity.java
@@ -3,75 +3,69 @@ package com.android.martyrapp.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.android.martyrapp.model.Martyr;
+import com.android.martyrapp.model.MartyrPojo;
+import com.android.martyrapp.model.MartyrResponse;
+import com.android.martyrapp.util.ApiClient;
+import com.android.martyrapp.util.ApiInterface;
 import com.android.martyrapp.util.Constants;
-import com.android.volley.Request;
+import com.android.martyrapp.util.Utility;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue queue;
     private Intent intent;
+    private List<MartyrPojo> martyrsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.nextBtn);
-        queue = Volley.newRequestQueue(this);
+//        queue = Volley.newRequestQueue(this);
         intent = new Intent(MainActivity.this, MartyrListActivity.class);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                Utility.showProgressDailog(MainActivity.this);
                 getMartyrDetails();
+//                Utility.dismissProgressDailog();
             }
         });
     }
     public void getMartyrDetails() {
         try {
-            JsonObjectRequest jsonObjectRequest =  new JsonObjectRequest(Request.Method.GET,
-                    Constants.API_URL,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                JSONObject martyrObj1 = response.getJSONObject("martyr");
-                                JSONArray martyrArray = martyrObj1.getJSONArray("martyr");
-                                for (int i = 0; i < martyrArray.length(); i++) {
-                                    JSONObject martyrObj = martyrArray.getJSONObject(i);
-                                    String martyrId = Integer.valueOf(martyrObj.getInt("martyrId")).toString();
-                                    String martyrName = martyrObj.get("martyrName").toString();
-                                    String martyrYear = martyrObj.get("martyrYear").toString();
-                                    String martyrImageName = martyrObj.get("martyrImageName").toString();
-                                    Martyr martyr = new Martyr();
-//                                    martyr.setGenre(martyrYear);
-//                                    martyr.setYear("");
-                                    martyr.setTitle(martyrName);
-                                    martyr.setImageName(martyrImageName);
-                                    intent.putExtra(martyrId,martyr);
-                                }
-                                startActivityForResult(intent,2);
-                            } catch (JSONException ex) {
-                                ex.printStackTrace();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
+            ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+
+            Call<MartyrResponse> call = apiService.getMartyrDetails(Constants.API_KEY_VAL);
+            call.enqueue(new Callback<MartyrResponse>() {
                 @Override
-                public void onErrorResponse(VolleyError error) {
-                    VolleyLog.d("Error:", error.getMessage());
+                public void onResponse(Call<MartyrResponse> call, Response<MartyrResponse> response) {
+
+                    int statusCode = response.code();
+                    martyrsList = response.body().getMartyrs().getMartyr();
+                    for(int i=0; i< martyrsList.size(); i++) {
+                        MartyrPojo martyrData =(MartyrPojo)martyrsList.get(i);
+                        Log.i(" test %%%%% ", martyrData.getMartyrImageName());
+                        intent.putExtra(martyrData.getMartyrId().toString(), martyrData);
+                    }
+                    startActivityForResult(intent,2);
+                }
+                @Override
+                public void onFailure(Call<MartyrResponse> call, Throwable t) {
+                    // Log error here since request failed
+                    Utility.dismissProgressDailog();
+                    Log.e("test", t.toString());
                 }
             });
-            queue.add(jsonObjectRequest);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
