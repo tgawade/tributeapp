4a26c2170ba2d0639e66d33dc339137e8d9e76e4
tgawade <tushar1577@gmail.com>
2017-11-13 21:26:52 +0530

    added retrofit

--- app/src/main/java/com/android/martyrapp/activity/MartyrListActivity.java
+++ app/src/main/java/com/android/martyrapp/activity/MartyrListActivity.java
@@ -14,30 +14,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.martyrapp.model.Martyr;
+import com.android.martyrapp.model.MartyrPojo;
+import com.android.martyrapp.model.MartyrResponse;
+import com.android.martyrapp.util.ApiClient;
+import com.android.martyrapp.util.ApiInterface;
 import com.android.martyrapp.util.Constants;
 import com.android.martyrapp.util.DividerItemDecoration;
 import com.android.martyrapp.util.Prefs;
 import com.android.martyrapp.util.RecyclerTouchListener;
-import com.android.volley.Request;
+import com.android.martyrapp.util.Utility;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+
 public class MartyrListActivity extends AppCompatActivity {
-    private List<Martyr> martyrList = new ArrayList<>();
+    private List<MartyrPojo> martyrList = new ArrayList<>();
+    private List<MartyrPojo> martyrsList = new ArrayList<>();
     private RecyclerView recyclerView;
     private MartyrAdapter mAdapter;
     private AlertDialog.Builder alertDialogBuilder;
@@ -62,9 +59,7 @@ public class MartyrListActivity extends AppCompatActivity {
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
-                Martyr martyr = martyrList.get(position);
-                //Toast.makeText(getApplicationContext(), martyr.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
-
+                MartyrPojo martyr = martyrList.get(position);
             }
             @Override
             public void onLongClick(View view, int position) {
@@ -80,7 +75,7 @@ public class MartyrListActivity extends AppCompatActivity {
             }
         });
     }
-    public void showInputDialog(final List<Martyr> martyrList) {
+    public void showInputDialog(final List<MartyrPojo> martyrList) {
         alertDialogBuilder = new AlertDialog.Builder(this);
         View view = getLayoutInflater().inflate(R.layout.dialog_view, null);
         final EditText newSearchEdt = (EditText) view.findViewById(R.id.searchEdt);
@@ -88,11 +83,6 @@ public class MartyrListActivity extends AppCompatActivity {
         alertDialogBuilder.setView(view);
         dialog = alertDialogBuilder.create();
         dialog.show();
-        if(queue != null){
-            queue.stop();
-        }
-        queue = Volley.newRequestQueue(this);
-        queue.start();
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,60 +91,54 @@ public class MartyrListActivity extends AppCompatActivity {
                     String search = newSearchEdt.getText().toString();
                     prefs.setSearch(search);
                     dialog.dismiss();
-                    try {
-                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                                Constants.API_URL_SEARCH + search,
-                                new Response.Listener<JSONObject>() {
-                                    @Override
-                                    public void onResponse(JSONObject response) {
-                                        try {
-                                            Log.d("showInputDialog" + new Date().getTime(),"in request");
-                                            JSONObject martyrObj1 = response.getJSONObject("martyr");
-                                            JSONArray martyrArray = martyrObj1.getJSONArray("martyr");
-
-                                            if(martyrArray!= null && martyrArray.length() == 0) {
-                                                Toast.makeText(getApplicationContext(), " Search string not found", Toast.LENGTH_SHORT).show();
-                                            } else {
-                                                martyrList.clear();
-                                                for (int i = 0; i < martyrArray.length(); i++) {
-                                                    JSONObject martyrObj = martyrArray.getJSONObject(i);
-                                                    String martyrId = Integer.valueOf(martyrObj.getInt("martyrId")).toString();
-                                                    String martyrName = martyrObj.get("martyrName").toString();
-                                                    String martyrYear = martyrObj.get("martyrYear").toString();
-                                                    String martyrImage = martyrObj.get("martyrImageName").toString();
-                                                    Martyr martyr = new Martyr();
-                                                    //martyr.setGenre(martyrYear);
-                                                    martyr.setTitle(martyrName);
-//                                                    martyr.setYear(martyrYear);
-                                                    martyr.setImageName(martyrImage);
-                                                    martyrList.add(martyr);
-                                                }
-                                                mAdapter.notifyDataSetChanged();
-                                            }
-                                        } catch (JSONException ex) {
-                                            ex.printStackTrace();
-                                        }
-                                    }
-                                }, new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                VolleyLog.d("Error:", error.getMessage());
-                            }
-                        });
-                        queue.add(jsonObjectRequest);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
+                    getMartyrDetailFilter(search);
                 }
             }
         });
     }
 
+    public void getMartyrDetailFilter(String searchKeyword) {
+        try {
+            Utility.showProgressDailog(MartyrListActivity.this);
+            ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+            Call<MartyrResponse> call = apiService.getMartyrDetailFilter(searchKeyword,Constants.API_KEY_VAL);
+            call.enqueue(new Callback<MartyrResponse>() {
+                @Override
+                public void onResponse(Call<MartyrResponse> call, retrofit2.Response<MartyrResponse> response) {
+
+                    int statusCode = response.code();
+                    martyrList.clear();
+                    martyrsList = response.body().getMartyrs().getMartyr();
+                    if(martyrsList != null && martyrsList.size() == 0) {
+                        Toast.makeText(getApplicationContext(), " Search string not found", Toast.LENGTH_SHORT).show();
+                    } else {
+                        for(MartyrPojo item: martyrsList) {
+                            MartyrPojo martyr = new MartyrPojo();
+//                          martyr.setGenre(martyrYear);
+                            martyr.setMartyrName(item.getMartyrName());
+//                          martyr.setYear(martyrYear);
+                            martyr.setMartyrImageName(item.getMartyrImageName());
+                            martyrList.add(martyr);
+                            Log.i(" test in details >>>> ", item.getMartyrName() );
+                        }
+                        Utility.dismissProgressDailog();
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }
+                @Override
+                public void onFailure(Call<MartyrResponse> call, Throwable t) {
+                    Log.e("test", t.toString());
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     private void prepareMartyrList(Bundle extras) {
             Set keySet = extras.keySet();
-
             for (int i = 0; i < keySet.size();i++) {
-                Martyr martyr = (Martyr) extras.get(Integer.toString(i));
+                MartyrPojo martyr = (MartyrPojo) extras.get(Integer.toString(i));
                 martyrList.add(martyr);
             }
             mAdapter.notifyDataSetChanged();
